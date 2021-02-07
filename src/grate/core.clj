88365823449d4e690bd5d31111dce54b33fe30a3
Todd Crone <twcrone@gmail.com>
2021-02-06 21:42:36 -0500

    Add methods for all three output formats

--- src/grate/core.clj
+++ src/grate/core.clj
@@ -8,10 +8,30 @@
   [records]
   (doseq [record records] (println record)))
 
+
+(defn output1
+  [records]
+  (println "*** Output 1 ***")
+  (print-records (map output/display-str
+                      (sort record/compare-on-gender-asc-then-last-name-asc records))))
+
+(defn output2
+  [records]
+  (println "*** Output 2 ***")
+  (print-records (map output/display-str
+                      (sort record/compare-on-birth-date-asc records))))
+
+(defn output3
+  [records]
+  (println "*** Output 3 ***")
+  (print-records(map output/display-str
+                     (sort record/compare-on-last-name-desc records))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Load file location from first argument and print reports"
   [& args]
-  (print-records
-    (map output/display-str
-         (sort record/compare-on-gender-asc-then-last-name-asc (records/load-from "test/test-file.txt")))))
+  (let [records (records/load-from (first args))]
+    (output1 records)
+    (output2 records)
+    (output3 records)))
 
