b9797451e86f356a51702396459d711aba1118a7
Todd Crone <twcrone@gmail.com>
2021-02-07 19:43:42 -0500

    Add cloverage plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[lein-cloverage "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
                  [compojure "1.6.1"]
