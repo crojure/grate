2078919b77a7181a6a6186df08e4671554f0b19a
Todd Crone <twcrone@gmail.com>
2021-02-07 23:32:27 -0500

    Add more unit tests for serializer code

--- test/grate/record/serializer_test.clj
+++ test/grate/record/serializer_test.clj
@@ -4,19 +4,23 @@
 
 (deftest test-format-date-str
   (testing "Format date string to M/D/YYYY for output"
-    (is (= "10/10/1970"
-          (format-date-str "1970-10-10")))))
+    (is (= "10/10/1970" (format-date-str "1970-10-10")))
+    (is (= "1/2/1970" (format-date-str "1970-01-02")))))
 
-(deftest test-format-date-str-single-digit-day-and-month
-  (testing "Format date string to M/D/YYYY for output"
-    (is (= "1/2/1970"
-           (format-date-str "1970-01-02")))))
-
-(deftest test-display-str
-  (testing "Format record for output"
-        (is (= "Crone,Todd,M,hazel,10/10/1970"
+(deftest test-to-csv
+  (testing "Serialize record for CSV output"
+        (is (= "Crone,Todd,M,purple,10/10/1970"
                (to-csv {:last-name      "Crone"
                         :first-name     "Todd"
                         :gender         "M"
-                        :favorite-color "hazel"
+                        :favorite-color "purple"
                         :date-of-birth  "1970-10-10"})))))
+
+(deftest test-to-json
+  (testing "Serialize record for JSON output"
+    (is (= "{\"last-name\":\"Crone\",\"first-name\":\"Todd\",\"gender\":\"M\",\"favorite-color\":\"purple\",\"date-of-birth\":\"10\\/10\\/1970\"}"
+           (to-json {:last-name      "Crone"
+                     :first-name     "Todd"
+                     :gender         "M"
+                     :favorite-color "purple"
+                     :date-of-birth  "1970-10-10"})))))
